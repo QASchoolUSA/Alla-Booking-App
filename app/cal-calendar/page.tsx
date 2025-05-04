27b9914010df270957541194d8120bed9233fdf0
kedrov <qaschoolusa@gmail.com>
2025-05-04 18:54:36 -0400

    Updated Cal.com to use like a React component.

--- app/cal-calendar/page.tsx
+++ app/cal-calendar/page.tsx
@@ -1,15 +1,10 @@
+"use client";
+import Cal from "@calcom/embed-react";
+
 export default function CalCalendarPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#EEEFF2]">
-      <div className="w-full max-w-2xl" style={{ height: "90vh" }}>
-        <iframe
-          src="https://cal.com/nikita-kedrov"
-          width="100%"
-          height="100%"
-          title="Book with Cal.com"
-          allow="camera; microphone; fullscreen; speaker"
-        />
-      </div>
+    <div className="">
+      <Cal calLink="nikita-kedrov" config={{ theme: "light" }}></Cal>
     </div>
   );
 }
